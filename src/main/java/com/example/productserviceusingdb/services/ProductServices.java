bd707e635d7d4b812543f690f1fb98de52533f2c
shmeera <meera2009@gmail.com>
2024-01-24 11:36:10 +0530

    modified code

--- src/main/java/com/example/productserviceusingdb/services/ProductServices.java
+++ src/main/java/com/example/productserviceusingdb/services/ProductServices.java
@@ -22,9 +22,10 @@ public class ProductServices implements IProduct {
     public Product GetProductById(Long id) throws NotFoundException {
         Product product = productRepository.findById(id).orElse(null);
         if(product==null)
-            throw new NotFoundException(" Product with id: " + id + " not found enter correct id");
+            throw new NotFoundException(" Testing Product with id: " + id + " not found enter correct id");
         else
              return product;
+
     }
 
     @Override
